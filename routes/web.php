49aaf1b3367f2bbb04cde80f4900e3915fa614dd
mudassirprnc <mudassirprnc@gmail.com>
2026-05-10 21:26:55 +0530

    updated web route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\GradeController;
+
 
 Route::get('/', function () {
     return view('welcome');
