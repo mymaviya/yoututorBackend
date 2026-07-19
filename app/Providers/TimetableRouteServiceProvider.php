d516738a477f272b603e2f6b54a0bca8c1978a10
Mudassir Husain <47500865+mymaviya@users.noreply.github.com>
2026-07-19 21:45:38 +0530

    Register weekly timetable editor routes

--- app/Providers/TimetableRouteServiceProvider.php
+++ app/Providers/TimetableRouteServiceProvider.php
@@ -29,6 +29,7 @@ class TimetableRouteServiceProvider extends ServiceProvider
                 require base_path('routes/parallel_groups.php');
                 require base_path('routes/timetable_entry_locks.php');
                 require base_path('routes/timetable_lifecycle.php');
+                require base_path('routes/timetable_editor.php');
                 require base_path('routes/timetable_generation_runs.php');
             });
     }
