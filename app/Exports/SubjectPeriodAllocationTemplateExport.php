01ca6412176629c6890e19c1a9dde536e75e8d96
Mudassir Husain <47500865+mymaviya@users.noreply.github.com>
2026-07-19 19:49:24 +0530

    Align subject allocation template with import fields

--- app/Exports/SubjectPeriodAllocationTemplateExport.php
+++ app/Exports/SubjectPeriodAllocationTemplateExport.php
@@ -3,9 +3,18 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithAutoFilter;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class SubjectPeriodAllocationTemplateExport implements FromArray, WithHeadings
+class SubjectPeriodAllocationTemplateExport implements
+    FromArray,
+    WithHeadings,
+    ShouldAutoSize,
+    WithAutoFilter,
+    WithStyles
 {
     public function headings(): array
     {
@@ -17,17 +26,29 @@ class SubjectPeriodAllocationTemplateExport implements FromArray, WithHeadings
             'Preferred Teacher',
             'Double Period',
             'Morning',
+            'Last Period',
             'Saturday',
+            'Optional',
             'Parallel',
             'Parallel Group Code',
             'Priority',
+            'Active',
         ];
     }
 
     public function array(): array
     {
         return [
-            ['English', 'major', 8, 2, '', 'Yes', 'Yes', 'No', 'No', '', 5],
+            ['English', 'major', 8, 2, '', 'Yes', 'Yes', 'No', 'No', 'No', 'No', '', 5, 'Yes'],
+        ];
+    }
+
+    public function styles(Worksheet $sheet): array
+    {
+        $sheet->freezePane('A2');
+
+        return [
+            1 => ['font' => ['bold' => true]],
         ];
     }
 }
